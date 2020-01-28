be2996bc542e56dd20527cd109b9945ba1fca21f
Edd <edward.abrahamsen-mills@safetyculture.io>
2020-01-28 15:15:00 +0000

    Fixed Table Creation Handling

--- safetyculture-sdk-python/tools/exporter/exporter.py
+++ safetyculture-sdk-python/tools/exporter/exporter.py
@@ -683,7 +683,8 @@ def load_config_settings(logger, path_to_config_file, docker_enabled):
             ALLOW_TABLE_CREATION: set_env_defaults('ALLOW_TABLE_CREATION', os.environ['ALLOW_TABLE_CREATION'], logger),
             ACTIONS_TABLE: 'iauditor_actions_data',
             ACTIONS_MERGE_ROWS: set_env_defaults('ACTIONS_MERGE_ROWS', os.environ['ACTIONS_MERGE_ROWS'], logger),
-            PREFERENCES: None
+            PREFERENCES: None,
+            FILENAME_ITEM_ID: None
         }
     else:
         config_settings = yaml.safe_load(open(path_to_config_file))
