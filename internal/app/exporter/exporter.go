62f62c6e4bafabbe606a4c85fd78ab11b1523c53
phanikumar <phanikumar.patchigolla@safetyculture.io>
2021-03-05 16:23:18 +1100

    [INTG-1670] Modified_after not being respected in json export

--- internal/app/exporter/exporter.go
+++ internal/app/exporter/exporter.go
@@ -9,5 +9,5 @@ import (
 type Exporter interface {
 	WriteRow(name string, row *json.RawMessage)
 	SetLastModifiedAt(modifiedAt time.Time)
-	GetLastModifiedAt() *time.Time
+	GetLastModifiedAt(modifiedAfter time.Time) *time.Time
 }
