ef5108b67e88944618e0ef6dbc7d43a1a4f3bb4e
phanikumar <phanikumar.patchigolla@safetyculture.io>
2021-03-22 12:16:23 +1100

    [INTG-1726] Fix tests

--- internal/app/config/config_test.go
+++ internal/app/config/config_test.go
@@ -16,6 +16,7 @@ func TestGetInspectionConfig(t *testing.T) {
 	viperConfig.Set("export.inspection.archived", "both")
 	viperConfig.Set("export.inspection.completed", "both")
 	viperConfig.Set("export.inspection.skip_ids", "1 2 3")
+	viperConfig.Set("export.inspection.limit", 2)
 
 	modifiedAfter, err := time.Parse("2006-01-02", "2010-12-20")
 	assert.Equal(t, nil, err)
@@ -25,4 +26,5 @@ func TestGetInspectionConfig(t *testing.T) {
 	assert.Equal(t, "both", actual.Archived)
 	assert.Equal(t, "both", actual.Completed)
 	assert.Equal(t, []string{"1", "2", "3"}, actual.SkipIDs)
+	assert.Equal(t, 2, actual.Limit)
 }
