8ff45f484aab3aa350c6975ce9ec0d0ced7fb0e0
HarryTranAU <58361130+HarryTranAU@users.noreply.github.com>
2021-04-21 09:10:17 +1000

    [INTG-1773] Smoke test for iauditor-exporter cmd (#113)
    Add test case to make sure no errors are thrown when "iauditor-exporter configure" command is run

---
+++ cmd/iauditor-exporter/cmd/configure_test.go
@@ -0,0 +1,20 @@
+package cmd_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/SafetyCulture/iauditor-exporter/cmd/iauditor-exporter/cmd"
+)
+
+func TestCommandConfigure_should_not_throw_error(t *testing.T) {
+	b := bytes.NewBufferString("")
+	cmd.RootCmd.SetOut(b)
+	cmd.RootCmd.SetArgs([]string{"configure"})
+	cmd.Execute()
+	_, err := ioutil.ReadAll(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
