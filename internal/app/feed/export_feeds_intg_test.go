052e85dd605d2a6729f2a06ba77c19bece31b3da
Brandon Cook <brandon.cook@safetyculture.io>
2022-06-06 08:35:39 +1000

    Fix integration tests

--- internal/app/feed/export_feeds_intg_test.go
+++ internal/app/feed/export_feeds_intg_test.go
@@ -50,7 +50,7 @@ func TestIntegrationDbCreateSchema_should_create_all_schemas(t *testing.T) {
 	filesEqualish(t, "mocks/set_1/schemas/template_permissions.csv", filepath.Join(exporter.ExportPath, "template_permissions.csv"))
 
 	filesEqualish(t, "mocks/set_1/schemas/sites.csv", filepath.Join(exporter.ExportPath, "sites.csv"))
-	filesEqualish(t, "mocks/set_1/outputs/site_members.csv", filepath.Join(exporter.ExportPath, "site_members.csv"))
+	filesEqualish(t, "mocks/set_1/schemas/site_members.csv", filepath.Join(exporter.ExportPath, "site_members.csv"))
 
 	filesEqualish(t, "mocks/set_1/schemas/users.csv", filepath.Join(exporter.ExportPath, "users.csv"))
 	filesEqualish(t, "mocks/set_1/schemas/groups.csv", filepath.Join(exporter.ExportPath, "groups.csv"))
