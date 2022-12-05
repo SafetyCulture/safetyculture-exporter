3b7ea095bde377790fed30ba68da7a63a5ba1cef
Gaurav Verma <gaurav.verma@safetyculture.io>
2022-12-05 15:11:02 +1100

    INTG-2252 - Initial commit for Exporter UI

---
+++ ui/frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import {Greet} from '../wailsjs/go/main/App.js'
+    import Page1 from './page1.svelte';
+  import page1 from "./page1.svelte"
+</script>
+
+<main>
+
+<Page1></Page1>
+  
+</main>
+
+<style>
+</style>
---
+++ ui/frontend/src/page1.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { ValidateApiKey } from "../wailsjs/go/main/App.js"
+
+    let isValid = false;
+    let apiKey;
+
+    function validate() {
+        isValid = false
+        ValidateApiKey(apiKey).then((result) => (isValid = result))
+    }
+</script>
+
+<div>
+    <h2>Enter your API key</h2>
+    <input class="input" id="value" type="text" size="60" bind:value={apiKey} />&nbsp;&nbsp;
+    {@debug apiKey}
+    <button class="btn" on:click={validate}>Validate</button>
+</div>
+
+{#if isValid}
+<h3>API key is valid</h3>
+{:else}
+<h3>API key is invalid</h3>
+{/if}
