4d73a099a1b44338e15582c80c09514bd798bb44
Mick Stanciu <mick.stanciu@safetyculture.io>
2022-12-13 16:04:53 +1100

    tuning

--- ui/frontend/src/pages/Welcome.svelte
+++ ui/frontend/src/pages/Welcome.svelte
@@ -5,9 +5,18 @@
 
 	let isValid = false;
 	let apiKey;
+	let buttonLabel = "Verify"
+	let displayBadApiKeyErr = false
+
 	function validate() {
 		isValid = false
-		ValidateApiKey(apiKey).then((result) => (isValid = result))
+		ValidateApiKey(apiKey).then((result) => {
+			isValid = result
+			if (isValid === false) {
+				buttonLabel = "Try again"
+				displayBadApiKeyErr = true
+			}
+		})
 	}
 </script>
 
@@ -16,28 +25,35 @@
 		<section class="welcome-header">
 			<img
 				id="welcome-page-logo"
-				src="../images/sc-logo.png"
+				src="../images/logo.png"
 				alt="SafetyCulture logo"
 			/>
 			<h1 class="welcome-title">Welcome to SafetyCulture Exporter</h1>
 		</section>
 		<section class="token-validation">
-			<div class="token-validation-text">
-				Generate an API token from your <a href="https://www.safetyculture.com" target="_blank" rel="noreferrer">SafetyCulture account.</a>
-			</div>
+			<div class="token-validation-text">Generate an API token from your SafetyCulture <span class="link">user profile</span>.</div>
 			<input
 				id="token-validation-input"
 				type="text"
 				placeholder="Enter API Token here"
 				bind:value={apiKey}
 			/>
-			<button class="token-verify-button" on:click={validate}>Verify</button>
 
-			{#if isValid}
-				<h3>API key is valid</h3>
-			{:else}
-				<h3>API key is invalid</h3>
+			{#if displayBadApiKeyErr}
+				<div class="error-block">
+					<div class="error-block-title">
+						Oops! We couldn't verify your token.
+					</div>
+					<div class="error-block-body">
+						Here's our diagnosis:
+						<ul>
+							<li>Your token might be expired. It expires after 30 days of not being used.</li>
+						</ul>
+					</div>
+				</div>
 			{/if}
+
+			<button class="token-verify-button" on:click={validate}>{buttonLabel}</button>
 		</section>
 
 		<section class="storage-info">
@@ -46,14 +62,14 @@
 					<img src="../images/round_exclamation_mark.png" alt="alert icon" width="20" height="20">
 				</div>
 				<div>
-					<div class="note-title">Important Note</div>
+					<div class="note-title">Before you continue</div>
 					<div class="note-body">All files (apart from SQL) you export will be stored in the same place on your computer or server as the SafetyCulture Exporter. If you want to change where your files get exported, please move the SafetyCulture Exporter file itself to that place.</div>
 				</div>
 			</div>
 		</section>
 	</section>
 	<section class="welcome-right-side">
-		<div class="image">
+		<div class="right-image">
 			<img src="../images/token_example.png" alt="example generating token">
 		</div>
 	</section>
