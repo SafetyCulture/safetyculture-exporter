2333a5f1da48db25185351c7786c3a553868e5c0
Mick Stanciu <mick.stanciu@safetyculture.io>
2023-03-29 10:03:02 +1100

    INTG-2739 small changes

--- cmd/safetyculture-exporter/cmd/root.go
+++ cmd/safetyculture-exporter/cmd/root.go
@@ -33,6 +33,7 @@ var RootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	fmt.Printf("SafetyCulture Exporter CLI version %s\n", version.GetVersion())
 	updateMsgChan := make(chan *update.ReleaseInfo)
 
 	go func() {
