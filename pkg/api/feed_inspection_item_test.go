474a0e41a5a9a987dbb49f049cc7deab5a3ff46b
Mick Stanciu <mick.stanciu@safetyculture.io>
2024-03-25 14:35:39 +1100

    PROBLEM-3017 add skip fields

--- pkg/api/feed_inspection_item_test.go
+++ pkg/api/feed_inspection_item_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/SafetyCulture/safetyculture-exporter/pkg/internal/feed"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/h2non/gock.v1"
 )
 
@@ -35,6 +36,38 @@ func TestInspectionItemFeedExport_should_export_rows_to_sql_db(t *testing.T) {
 	assert.Nil(t, resp.Error)
 
 	assert.Equal(t, 5, len(rows))
+}
+
+func TestInspectionItemFeedExport_should_ignore_media_hyper(t *testing.T) {
+	exporter, err := getInmemorySQLExporter("")
+	assert.NoError(t, err)
+	apiClient := GetTestClient()
+	initMockInspectionItemsFeed(apiClient.HTTPClient())
+
+	inspectionItemFeed := feed.InspectionItemFeed{
+		SkipIDs:       []string{},
+		ModifiedAfter: time.Now(),
+		TemplateIDs:   []string{},
+		Archived:      "both",
+		Completed:     "both",
+		Incremental:   true,
+		SkipFields:    []string{"some_unknown_field", "media_hypertext_reference", "some_unknown_field"},
+	}
+
+	err = inspectionItemFeed.Export(context.Background(), apiClient, exporter, "")
+	assert.NoError(t, err)
+
+	var rows []feed.InspectionItem
+	resp := exporter.DB.Table("inspection_items").Scan(&rows)
+	assert.Nil(t, resp.Error)
+
+	require.Equal(t, 7, len(rows))
+	for _, row := range rows {
+		if row.ID == "audit_493215ad4986455688bfebb8de93197e_5aa3170b-b1a0-4a99-8425-5417cea967b7" {
+			assert.Empty(t, row.MediaHypertextReference)
+		}
+	}
+
 }
 
 func TestInspectionItemFeedExportWithMedia(t *testing.T) {
